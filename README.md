58a5bf8b44fc8fabc83926b5bd9aea156102da8d
olegskl <sklyanchuk@gmail.com>
2014-12-28 13:51:50 +0100

    feat(operators): add modulus, comparison, bitwise

--- README.md
+++ README.md
@@ -36,6 +36,28 @@ calculator('1 2 + 3 -'); // 0
 calculator('1 2 + 3 *'); // 9
 ```
 
+### Available operators
+
+All operators replicate the default JavaScript behavior except `min` and `max`
+which are limited to 2 arguments to satisfy the default arity of RPN calculator.
+
+operator | operation                    | example
+:-------:|------------------------------|------------
+`+`      | addition                     | `1 2 +` = 3
+`-`      | subtraction                  | `1 2 -` = -1
+`*`      | multiplication               | `2 3 *` = 6
+`/`      | division                     | `7 2 /` = 3.5
+`%`      | modulus                      | `12 5 %` = 2
+`pow`    | exponentiation               | `7 2 pow` = 49
+`max`    | maximum *of two*             | `1 2 max` = 2
+`min`    | minimum *of two*             | `1 2 min` = 1
+`&`      | bitwise AND                  | `15 9 &` = 9
+`|`      | bitwise OR                   | `15 9 |` = 15
+`^`      | bitwise XOR                  | `15 9 ^` = 6
+`<<`     | left shift                   | `9 2 <<` = 36
+`>>`     | sign-propagating right shift | `9 2 >>` = 2
+`>>>`    | zero-filled right shift      | `9 2 >>>` = 2
+
 ## Tests
 
 ```Bash
--- test/operators.test.js
+++ test/operators.test.js
@@ -16,6 +16,8 @@ describe('operators', function () {
         expect(operators).to.be.an('object');
     });
 
+    // Arithmetic operators:
+
     it('should have a `+` method that sums', function () {
         expect(operators).to.have.property('+').that.is.a('function');
         expect(operators['+'](1, 2)).to.equal(3);
@@ -33,7 +35,62 @@ describe('operators', function () {
 
     it('should have a `/` method that divides', function () {
         expect(operators).to.have.property('/').that.is.a('function');
-        expect(operators['/'](6, 3)).to.equal(2);
+        expect(operators['/'](7, 2)).to.equal(3.5);
+    });
+
+    it('should have a `%` method that returns a modulus', function () {
+        expect(operators).to.have.property('%').that.is.a('function');
+        expect(operators['%'](12, 5)).to.equal(2);
+    });
+
+    it('should have a `pow` method that exponentiates', function () {
+        expect(operators).to.have.property('pow').that.is.a('function');
+        expect(operators.pow(7, 2)).to.equal(49);
+    });
+
+    // Comparison operators:
+
+    it('should have a `max` method that returns largest of args', function () {
+        expect(operators).to.have.property('max').that.is.a('function');
+        expect(operators.max(1, 2)).to.equal(2);
+    });
+
+    it('should have a `min` method that returns smallest of args', function () {
+        expect(operators).to.have.property('min').that.is.a('function');
+        expect(operators.min(1, 2)).to.equal(1);
+    });
+
+    // Bitwise operators:
+
+    it('should have a `&` method that performs bitwise AND', function () {
+        expect(operators).to.have.property('&').that.is.a('function');
+        expect(operators['&'](15, 9)).to.equal(9); // 1111 & 1001 = 1001
+    });
+
+    it('should have a `|` method that performs bitwise OR', function () {
+        expect(operators).to.have.property('|').that.is.a('function');
+        expect(operators['|'](15, 9)).to.equal(15); // 1111 & 1001 = 1111
+    });
+
+    it('should have a `^` method that performs bitwise XOR', function () {
+        expect(operators).to.have.property('^').that.is.a('function');
+        expect(operators['^'](15, 9)).to.equal(6); // 1111 & 1001 = 0110
+    });
+
+    it('should have a `<<` method that shifts left', function () {
+        expect(operators).to.have.property('<<').that.is.a('function');
+        expect(operators['<<'](9, 2)).to.equal(36); // 1001 -> 100100
+    });
+
+    it('should have a `>>` method that shifts right with sign', function () {
+        expect(operators).to.have.property('>>').that.is.a('function');
+        expect(operators['>>'](9, 2)).to.equal(2); // 1001 -> 10
+        expect(operators['>>'](-9, 2)).to.equal(-3); // -1001 -> -11
+    });
+
+    it('should have a `>>>` method that shifts right', function () {
+        expect(operators).to.have.property('>>>').that.is.a('function');
+        expect(operators['>>>'](19, 2)).to.equal(4); // 10011 -> 100
     });
 
 });
